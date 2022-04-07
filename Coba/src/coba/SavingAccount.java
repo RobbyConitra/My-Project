4b090be146b1634841c4903cff8da03868055d12
RobbyConitra <97679710+RobbyConitra@users.noreply.github.com>
2022-04-07 21:40:58 +0700

    Update SavingAccount.java

--- Coba/src/coba/SavingAccount.java
+++ Coba/src/coba/SavingAccount.java
@@ -1,13 +1,5 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package coba;
 
-/**
- *
- * @author Thea Jessica
- */
 public class SavingAccount {
     public double balance;
     public double interestRate;
