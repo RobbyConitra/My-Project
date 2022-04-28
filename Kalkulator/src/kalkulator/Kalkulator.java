7b96c7b6488c77bde66cea1389a2203719164351
RobbyConitra <robbyconitra165@gmail.com>
2022-04-28 21:07:02 +0700

    kalku 2

--- Kalkulator/src/kalkulator/Kalkulator.java
+++ Kalkulator/src/kalkulator/Kalkulator.java
@@ -6,30 +6,32 @@ public class Kalkulator {
 
     public static void main(String[] args) {
         int kode;
-        double a,b,c,f,g,h;
         Scanner scan = new Scanner (System.in);
+        Kalku kalku = new Kalku ();
         
         System.out.println("--- Kalkulator ---");
         System.out.println("1. Penjumlahan");
         System.out.println("2. Pengurangan");
         System.out.println("3. Perkalian");
-        System.out.println("4. pembagian");
+        System.out.println("4. Pembagian");
         System.out.print("Pilihan : ");
         kode = scan.nextInt();
         
         switch(kode){
             case 1 :
-                System.out.println("--- Penjumlahan ---");
-                System.out.print("Masukkan angka pertama : ");
-                a = scan.nextDouble();
-                System.out.print("Masukkan angka kedua : ");
-                b = scan.nextDouble();
-                c = a+b;
-                System.out.println("Hasil : "+c);
+                kalku.tambah();
                 break;
             case 2 :
+                kalku.kurang();
+                break;
+            case 3 :
+                kalku.kali();
+                break;
+            case 4 :
+                kalku.bagi();
+                break;
+            default : System.out.println("ERROR!!!");
                 
-                        
         }
         
     }
